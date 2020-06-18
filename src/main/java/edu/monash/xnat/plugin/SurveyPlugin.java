db35e105f83239705d0ccbe62d75702e23179916
manishkumr <rishimanish123@gmail.com>
2020-06-18 13:38:31 +1000

    Adding survey data type

--- src/main/java/edu/monash/xnat/plugin/SurveyPlugin.java
+++ src/main/java/edu/monash/xnat/plugin/SurveyPlugin.java
@@ -1,10 +1,18 @@
 package edu.monash.xnat.plugin;
 
+import org.nrg.framework.annotations.XnatDataModel;
 import org.nrg.framework.annotations.XnatPlugin;
+import org.nrg.xdat.bean.SurveySurveycollectionBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-@XnatPlugin(value = "surveyPlugin", name = "XNAT Survey Plugin")
+@XnatPlugin(value = "surveyPlugin",
+        name = "XNAT Survey Plugin",
+        dataModels = @XnatDataModel(value = SurveySurveycollectionBean.SCHEMA_ELEMENT_NAME,
+        singular = "Survey",
+        plural = "Surveys",
+        code = "SV")
+)
 @ComponentScan({
         "edu.monash.xnat.plugin.preferences",
         "edu.monash.xnat.plugin.xapi"
